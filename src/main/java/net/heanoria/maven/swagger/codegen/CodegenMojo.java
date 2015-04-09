5658048b6459303619f4e06eed8b64c03004515b
Richou <Richard@Lutty>
2015-04-09 22:21:12 +0200

    First commit

---
+++ src/main/java/net/heanoria/maven/swagger/codegen/CodegenMojo.java
@@ -0,0 +1,90 @@
+package net.heanoria.maven.swagger.codegen;
+
+import com.wordnik.swagger.codegen.ClientOptInput;
+import com.wordnik.swagger.codegen.ClientOpts;
+import com.wordnik.swagger.codegen.CodegenConfig;
+import com.wordnik.swagger.codegen.DefaultGenerator;
+import com.wordnik.swagger.models.Swagger;
+import io.swagger.parser.SwaggerParser;
+import net.heanoria.maven.swagger.codegen.generator.JavaClientCodegenerator;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.util.ServiceLoader;
+
+import static java.util.ServiceLoader.load;
+
+@Mojo(name = "generate")
+public class CodegenMojo extends AbstractMojo {
+
+
+    private static final String TEMPLATE_DIR_PARAM = "templateDir";
+
+    @Parameter(property = "generate.lang", defaultValue = "java2")
+    private String lang = null;
+
+    @Parameter(property = "generate.outputDirectory", defaultValue = "")
+    private String output = null;
+
+    @Parameter(property = "generate.templateDir", defaultValue = "")
+    private String templateDir = null;
+
+    @Parameter(property = "generate.spec", defaultValue = "")
+    private String spec = null;
+
+    @Parameter(property = "generate.invokerPackage", defaultValue = "io.swagger.client")
+    private String invokerPackage = null;
+
+    @Parameter(property = "generate.groupId", defaultValue = "io.swagger")
+    private String groupId = null;
+
+    @Parameter(property = "generate.artifactId", defaultValue = "swagger-java-client")
+    private String artifactId = null;
+
+    @Parameter(property = "generate.artifactVersion", defaultValue = "1.0.0")
+    private String artifactVersion = null;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        ClientOptInput input = new ClientOptInput();
+
+        CodegenConfig config = forName(lang);
+        if(config instanceof JavaClientCodegenerator) {
+            JavaClientCodegenerator javaClientCodegenerator = (JavaClientCodegenerator) config;
+            javaClientCodegenerator.setInvokerPackage(invokerPackage);
+            javaClientCodegenerator.setGroupId(groupId);
+            javaClientCodegenerator.setArtifactId(artifactId);
+            javaClientCodegenerator.setArtifactVersion(artifactVersion);
+            config = javaClientCodegenerator;
+        }
+        config.setOutputDir(new File(output).getAbsolutePath());
+
+        if (null != templateDir) {
+            config.additionalProperties().put(TEMPLATE_DIR_PARAM, new File(templateDir).getAbsolutePath());
+        }
+
+        input.setConfig(config);
+
+        Swagger swagger = new SwaggerParser().read(spec, input.getAuthorizationValues(), true);
+        new DefaultGenerator().opts(input.opts(new ClientOpts()).swagger(swagger)).generate();
+    }
+
+    private CodegenConfig forName(String name) {
+        ServiceLoader<CodegenConfig> loader = load(CodegenConfig.class);
+        for (CodegenConfig config : loader) {
+            if (config.getName().equals(name)) {
+                return config;
+            }
+        }
+
+        // else try to load directly
+        try {
+            return (CodegenConfig) Class.forName(name).newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't load config class with name ".concat(name), e);
+        }
+    }
+}
---
+++ src/main/java/net/heanoria/maven/swagger/codegen/generator/JavaClientCodegenerator.java
@@ -0,0 +1,30 @@
+package net.heanoria.maven.swagger.codegen.generator;
+
+import com.wordnik.swagger.codegen.CodegenConfig;
+import com.wordnik.swagger.codegen.languages.JavaClientCodegen;
+
+public class JavaClientCodegenerator extends JavaClientCodegen implements CodegenConfig {
+
+    @Override
+    public String getName() {
+        return "java2";
+    }
+
+
+    public void setInvokerPackage(String invokerPackage) {
+        this.invokerPackage = invokerPackage;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public void setArtifactVersion(String artifactVersion) {
+        this.artifactVersion = artifactVersion;
+    }
+
+}
